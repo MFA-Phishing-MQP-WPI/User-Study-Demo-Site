40978d879a0cfb1c53307d364a286fcb26102670
Ian Poulsen <ianpoulsen26@gmail.com>
2025-01-21 13:17:02 -0500

    fix images

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const path = require("path");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "src/pages")))
-app.use(express.static(path.join(__dirname,"public/images")));
 app.use(express.static(path.join(__dirname,"public")));
 app.use(express.json());
 
