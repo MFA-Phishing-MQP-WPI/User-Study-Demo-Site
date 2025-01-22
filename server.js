e2c8f62456dfcd90379d4d350c6fb98f6ab94687
Ian Poulsen <ianpoulsen26@gmail.com>
2025-01-22 14:24:46 -0500

    Change image paths

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const path = require("path");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "docs")))
-app.use(express.static(path.join(__dirname,"images")));
 app.use(express.json());
 
 app.get("/", (req, res) => {
